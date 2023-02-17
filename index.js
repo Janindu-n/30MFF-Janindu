c63af15d30689dac6f608a4a46666ad47deddde5
Janindu Nanayakkara <96259895+Janindu-n@users.noreply.github.com>
2023-02-17 19:47:32 +0400

    fixed layout

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//A function to make the cards turn. The eventlistner wait for user input (a click) and then turns the card.
 window.onload = function() {
     const cards = document.querySelectorAll(".card");
     for (let card of cards) {
